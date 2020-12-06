2ac3bd9e7d137eb6dc55c7294b0cb3a39d36c0e8
Ibrahim Moussafia <73690252+Ibrahim233dd@users.noreply.github.com>
2020-12-06 18:43:33 +0100

    Add files via upload

--- filtrar-covid.cpp
+++ filtrar-covid.cpp
@@ -1,15 +1,118 @@
-
 #include <stdio.h>
 
 int main() {
-   int contador = 0;
+   int contador = 0, cit = 0;
    char caracter;
-
+   
    scanf("%c", &caracter);
-   while( caracter != '#' ) {
+   while( caracter != 'B' && cit != 0 ) {
       contador = contador+1;
       scanf("%c", &caracter);
    }
-   printf("Resultat %i\n", contador);
-   printf("#");
-}
+   printf("B");
+   cit++;
+   if( cit = 1 ){ 
+      scanf("%c", &caracter);
+      while( caracter != 'a' && cit != 1 && cit != 0 ) {
+      contador = contador+1;
+      scanf("%c", &caracter);
+      }
+      printf("a");
+      cit++;
+      if( cit = 2 ){
+        scanf("%c", &caracter);
+        while( caracter != 'l' && cit != 2 && cit != 1 && cit != 0 ) {
+        contador = contador+1;
+        scanf("%c", &caracter);
+        }
+        printf("l");
+        cit++;
+        if( cit = 3 ){
+            scanf("%c", &caracter);
+            while( caracter != 'a' && cit != 3 && cit != 2 && cit != 1 && cit != 0 ) {
+            contador = contador+1;
+            scanf("%c", &caracter);
+            }
+            printf("a");
+            cit++;
+            if( cit = 4 ){
+                 scanf("%c", &caracter);
+                 while( caracter != 'g' && cit != 4 && cit != 3 && cit != 2 && cit != 1 && cit != 0 ) {
+                 contador = contador+1;
+                 scanf("%c", &caracter);
+                 }
+                 printf("g");
+                 cit++;
+                 if( cit = 5 ){
+                    scanf("%c", &caracter);
+                    while( caracter != 'u' && cit != 5 && cit != 4 && cit != 3 && cit != 2 && cit != 1 && cit != 0 ) {
+                    contador = contador+1;
+                    scanf("%c", &caracter);
+                    }
+                    printf("u");
+                    cit++;
+                    if ( cit = 6 ){
+                        scanf("%c", &caracter);
+                        while( caracter != 'e' && cit != 6 && cit != 5 && cit != 4 && cit != 3 && cit != 2 && cit != 1 && cit != 0 ) {
+                        contador = contador+1;
+                        scanf("%c", &caracter);
+                        }
+                        printf("e");
+                        cit++;
+                        if( cit = 7 ){
+                           scanf("%c", &caracter);
+                           while( caracter != 'r' && cit != 7 && cit != 6 && cit != 5 && cit != 4 && cit != 3 && cit != 2 && cit != 1 && cit != 0 ) {
+                           contador = contador+1;
+                           scanf("%c", &caracter);
+                           }
+                           printf("r");
+                           cit++;
+                           if( cit = 8 ){ 
+                              scanf("%c", &caracter);
+                              while( caracter != ',' cracter&& cit != 8 && cit != 7 && cit != 6 && cit != 5 && cit != 4 && cit != 3 && cit != 2 && cit != 1 && cit != 0 ) {
+                              contador = contador+1;
+                              scanf("%c", &caracter);
+                              cit = 0;
+                              }
+                              printf(",");
+                              cit++;
+                              while( caracter != '\n' && cit > 8) {
+                              scanf("%c", &caracter);
+                              printf("%c", caracter);
+                              } 
+                              cit = 0;
+                              scanf("%c", &caracter);
+                              
+  }else{
+     cit = 0 ;
+     scanf("%c", &caracter);
+     
+  };}else{
+     cit = 0 ;
+     scanf("%c", &caracter);
+  };}else{
+     cit = 0 ;
+     scanf("%c", &caracter);
+  };}else{
+     cit = 0 ;
+     scanf("%c", &caracter);
+  };}else{
+     cit = 0 ;
+     scanf("%c", &caracter);
+  };}else{
+     cit = 0 ;
+     scanf("%c", &caracter);
+
+  };}else{
+     cit = 0 ;
+     scanf("%c", &caracter);
+  };}else{
+     cit = 0 ;
+     scanf("%c", &caracter);};
+  }
+   
+   
+   
+   
+   
+   
--- mitjana-covid.cpp
+++ mitjana-covid.cpp
@@ -1,13 +1,59 @@
-
 #include <stdio.h>
 
 int main() {
-   int acumulador = 0, valor;
-
+   int acumulador = 0, valor, valor1, valor2, valor3, valor4, cit = 0 ;
+   
    scanf("%i", &valor);
-   while( valor != 0 ) {
+   while( valor != 0 && cit = 0 ) {
       acumulador = acumulador + valor;
-      scanf("%i", &valor);
+      scanf("%i", &valor1);
+      cit++;
    }
-   printf("Resultat %i\n", acumulador);
-}
+   cit = 0;
+   scanf("%i", &valor);
+   printf("%i\n", acumulador);
+   
+   while( valor1 != 0 && cit = 1 ) {
+      acumulador = acumulador + valor1;
+      scanf("%i", &valor2);
+      cit++;
+   }
+   cit = 1;
+   scanf("%i", &valor1);
+   printf("%i\n", acumulador);
+   
+   while( valor2 != 0 ) {
+      acumulador = acumulador + valor2;
+      scanf("%i", &valor3);
+      cit++;
+   }
+   cit = 2;
+   scanf("%i", &valor2);
+   printf("%i\n", acumulador);
+   while( valor3 != 0 ) {
+      acumulador = acumulador + valor3;
+      scanf("%i", &valor4);
+      cit++;
+   }
+   cit = 0;
+   scanf("%i", &valor);
+   printf("%i\n", acumulador);
+   while( valor4 != 0 ) {
+      acumulador = acumulador + valor4;
+      scanf("%i", &valor5);
+   }
+   cit = 0;
+   scanf("%i", &valor);
+   printf("%i\n", acumulador);
+   while( valor5 != 0 ) {
+      acumulador = acumulador + valor5;
+      printf("%i\n", acumulador)
+   }
+   cit = 0;
+   scanf("%i", &valor);
+   printf("%i\n", acumulador);
+   ;}
+     
+
+
+
